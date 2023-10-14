d71bf9cf798140011601e5a0bdf05f235461c32f
houhou46 <122450870+houhou46@users.noreply.github.com>
2023-10-14 22:45:57 +0200

    projet final

--- assets/script.js
+++ assets/script.js
@@ -1,65 +1,59 @@
 const slides = [
+	// 0
 	{
-		"image":"slide1.jpg",
-		"src" : './assets/images/slideshow/slide1.jpg' , "alt": 'banniereprint-it',
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"src": './assets/images/slideshow/slide1.jpg', "alt": 'banniereprint-it',
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
+	// 1
 	{
-		"image":"slide2.jpg",
-		"src" : './assets/images/slideshow/slide2.jpg' , "alt": 'banniereprint-it',
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"src": './assets/images/slideshow/slide2.jpg', "alt": 'banniereprint-it',
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
+	// 2
 	{
-		"image":"slide3.jpg",
-		"src" : './assets/images/slideshow/slide3.jpg' , "alt": 'banniereprint-it',
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"src": './assets/images/slideshow/slide3.jpg', "alt": 'banniereprint-it',
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
+	// 3
 	{
-		"image":"slide4.png",
-		"src" : './assets/images/slideshow/slide4.jpg' , "alt": 'banniereprint-it',
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"src": './assets/images/slideshow/slide4.png', "alt": 'banniereprint-it',
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
-let position = 0
-const images = document.getElementById("banners")
-
-
 // Cherche et enregistre l'élément sur lequel on veut un event listener dans la variable globale (pour la flèche gauche en premier puis droite)
-let arrowClickLeft = document.getElementById("left")
-console.log(arrowClickLeft)
+const arrowClickRight = document.getElementById("right")
+const arrowClickLeft = document.getElementById("left")
+const images = document.getElementById("banners")
+const tagLine = document.getElementById("text")
+const dots = document.getElementsByClassName('dot')
+let position = 0
+
 
 // Création de l'event listener( fonction chargé d'écouter un évenement).La fonction n'a pas de nom elle est anonyme
 arrowClickLeft.addEventListener("click", () => {
 	position--
 	if (position < 0) {
-		position = slides.length
+		position = slides.length - 1
 	}
 	images.src = slides[position].src
-	console.log(position)
-	console.log("clic gauche")
+	tagLine.innerHTML = slides[position].tagLine
 })
 
-let arrowClickRight = document.getElementById("right")
-console.log(arrowClickRight)
-arrowClickRight.addEventListener("click",() => {
-	console.log("clic droit")
+
+arrowClickRight.addEventListener("click", arrowRight)
+
+function arrowRight() {
+	dots[position].classList.remove('dot_selected')
 	position++
-	if (position > slides.length) {
+	if (position >= slides.length) {
 		position = 0
 	}
 	images.src = slides[position].src
-	console.log(position)
-	console.log(slides[position]) 
-})
-
-
-// for (let i = 0; i < arrowClickRight.length; i++) {
-	//console.log(arrowClickLeft[i])
-
-
-
-
-
-
+	tagLine.innerHTML = slides[position].tagLine
+	dots[position].classList.add('dot_selected')
+}
 
